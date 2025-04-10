48daf0b79f61c01c59ac28a66819fced10c16310
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-04-09 23:56:53 -0300

    added infinite scroll

--- script.js
+++ script.js
@@ -38,5 +38,27 @@ const showPosts = async () => {
   });
 };
 
+// Show loader & fetch more posts
+const showLoading = () => {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+};
+
 // Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
