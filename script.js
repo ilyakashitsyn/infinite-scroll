74302f0179f6e94a4f48650ff6a86084ba50c51c
Ilya Kashitsyn <ilyakashitsyn@gmail.com>
2025-04-09 22:23:13 -0300

    initial posts from api

---
+++ script.js
@@ -0,0 +1,42 @@
+const postsContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 3;
+let page = 1;
+
+// Fetch posts from API
+const getPosts = async () => {
+  try {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+    );
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching posts:', error);
+  }
+};
+
+// Show posts in DOM
+const showPosts = async () => {
+  const posts = await getPosts();
+
+  posts.forEach(post => {
+    const postEl = document.createElement('div');
+    postEl.classList.add('post');
+    postEl.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+    `;
+
+    postsContainer.appendChild(postEl);
+  });
+};
+
+// Show initial posts
+showPosts();
